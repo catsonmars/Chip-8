791ffa354c84efd4dae8ae08820bc1d4ca8ea8ed
catsonmars <meganvineyard83@gmail.com>
2023-04-02 01:18:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("Is this main? : {}".format(__name__))
+from timeit import default_timer as timer
 import chip
 import pygame
 import time
@@ -11,23 +12,36 @@ def main():
     pygame.display.set_caption("Chip 8")
 
     #define pygame window namne
-    window = pygame.display.set_mode((640,480)) #window is a surface object
+    window = pygame.display.set_mode((640,480))
     rects = []  # list of rects "sprites" that will be drawn
     # load game into memory
     memory = chip.Memory(pygame, window,rects) #instantializes chip 8 class
 
     memory.loadGame()
     runProgram = True
+
+    #def draw():
+                                                #  x   y   len   height
+    #    pygame.draw.rect(window, (233,21,123), (1095, 222, 100, 100))
     #while runProgram:  #pygame window loop
-    while runProgram:
+    for x in range(0, 2215):
 
         #ensure 60hz display
-        start = time.time()
-        memory.emulateCycle(start, rects) #calls chip8 emulation
+        start = timer()
+
+        rects = memory.emulateCycle(start,rects) #calls chip8 emulation
+
 
         for event in pygame.event.get(): #run pygame loop
             if event.type == pygame.QUIT:
                 runProgram = False
+
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
